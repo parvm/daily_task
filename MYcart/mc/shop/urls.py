432c2c2fb8e19394d9dd10dc14825ad5057d61ff
parvm <parv.mittal@protonshub.in>
2020-08-31 12:44:31 +0530

    my Django project

---
+++ MYcart/mc/shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="Track your order"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.prodview, name="Product view"),
+    path("checkout/", views.checkout, name="Checkout")
+
+
+]
